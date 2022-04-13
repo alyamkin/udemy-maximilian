2c10be517df94b67bde4fd6150214cdc5b8b15c2
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2022-04-12 21:44:56 -0400

    Add 13-class-based-cmp

---
+++ src/components/UserFinder.js
@@ -0,0 +1,78 @@
+import { Fragment, useState, useEffect, Component } from "react";
+
+import UsersContext from "./store/user-context";
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+import ErrorBoundary from "./ErrorBoundary";
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
+
+  // componentDidMount life cycle component will runs only once. We don't need if statament to check the state
+  componentDidMount() {
+    //send http request...
+    this.setState({ filteredUsers: this.context.users });
+  }
+
+  // Implementation useEffect hook for class-based components
+  componentDidUpdate(prevProps, prevState) {
+    // If statement allows to avoid infinite loop
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
+// };
+
+export default UserFinder;
