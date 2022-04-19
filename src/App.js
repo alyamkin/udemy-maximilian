f39a2096fbf9a7d19be2b8d7d22169e47351a4f5
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2022-04-18 21:42:03 -0400

    Add 15 building custom react hook

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Fragment, useEffect, useState, useCallback } from "react";
+
+import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: `https://custom-hooks-tasks-57177-default-rtdb.firebaseio.com/tasks.json`,
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+      ;
+    </Fragment>
+  );
+}
+
+export default App;
