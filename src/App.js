ad1ceaa893899d3bd9c2b33c5d31c434e4e91ab3
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2022-06-21 19:58:28 -0400

    Add 24 animation

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Transition from "react-transition-group/transition";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    modalIsOpened: false,
+    showBlock: false,
+  };
+
+  showModal = () => {
+    this.setState({ modalIsOpened: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpened: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={300}
+          mountOnEntered
+          unmountOnExit
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExit={() => console.log("onExit")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExited")}
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+
+        <Modal closed={this.closeModal} show={this.state.modalIsOpened} />
+
+        <Backdrop show={this.state.modalIsOpened} />
+
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
